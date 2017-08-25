bdb6ea71f54fdba78248510120c7baa34f8d5a8a
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-25 18:33:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function createTemplate(data){
         
             <head>
                 <title>
-                    $(title)
+                    ${title}
                 </title>
                 <meta name="viewport" content="width=device-width , initial-scale=1" />
                 <link href="/ui/style.css" rel="stylesheet" />
@@ -43,7 +43,7 @@ function createTemplate(data){
                     </div>    
                     <hr/>
                     <h3>
-                        $(heading)
+                        ${heading}
                     </h3>
                     
                     <div>
@@ -51,7 +51,7 @@ function createTemplate(data){
                     </div>
                     
                     <div>
-                        $(content)
+                        ${content}
                     </div>
                 </div>
             
