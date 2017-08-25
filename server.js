551e8604f56617d363a14101686522288c62b510
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-25 19:28:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == content object for article-one
