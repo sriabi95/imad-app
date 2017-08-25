7e9956fcebc58e483962ac98a625e07ac4fe0304
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-25 18:29:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,8 +57,8 @@ function createTemplate(data){
             
             </body>    
             
-        </html>`
-    ;
+        </html>`;
+    return htmlTemplate;
 }
 
 
@@ -75,7 +75,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
