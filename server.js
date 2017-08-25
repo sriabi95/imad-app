9b84af022c05e26bc607dfb95e71a71b39729741
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-25 18:25:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : 'article-one',
+    heading : 'article-one',
+    content : `<p>
+                This is the content of my first article.
+            </p>
+            <p>
+                This is the content of my first article.
+            </p>
+            <p>
+                This is the content of my first article.
+            </p>`
+    
+};
+
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = 
+        `<html>
+        
+            <head>
+                <title>
+                    $(title)
+                </title>
+                <meta name="viewport" content="width=device-width , initial-scale=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            
+            <body>
+                <div class="container">
+                    <div>
+                        <a href = "/">home</a>
+                    </div>    
+                    <hr/>
+                    <h3>
+                        $(heading)
+                    </h3>
+                    
+                    <div>
+                        August 25th, 2017
+                    </div>
+                    
+                    <div>
+                        $(content)
+                    </div>
+                </div>
+            
+            </body>    
+            
+        </html>`
+    ;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
