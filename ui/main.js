5cd6d940f29204d43d4aca3eb06fcd29b0099485
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-26 19:24:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,3 @@
-/*console.log('Loaded!');
-//change the text of main_text div
-var element = document.getElementById("main_text");
-element.innerHTML = "New Value";
-
-//move the image
-var img = document.getElementById("abi");
-var marginLeft = 0;
-
-function moveRight (){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
-//    img.style.marginLeft = '100px';
-};*/
-
-/*var counter = 0;
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-   //make a request to the counter endpoint
-   
-   //capture the response and store it in a variable
-   
-   //render the variable in the correct span
-   counter = counter + 1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
-};*/
 
 var button = document.getElementById('counter');
 
@@ -53,14 +21,32 @@ button.onclick = function(){
 
 var submit = document.getElementbyId('submit_btn');
 submit.onclick = function(){
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementbyId('namelist');
-    ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+     request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status == 200){      //200 is the successful response
+                /*var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();*/
+                var nameInput = document.getElementById('name');
+                var name = nameInput.value;
+                
+                var names = request.responseText;
+                names = JSON.parse(names);
+                
+                var list = '';
+                for(var i=0; i<names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementbyId('namelist');
+                ul.innerHTML = list;
+            }
+        } 
+     };
+    //make a request
+    request.open('GET','http://sriabi95.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+    
+    
 };
 
