87e681f5cb140243588769a24bb60a92f17891ce
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-26 17:17:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,6 +48,18 @@ button.onclick = function(){
     //make a request
     request.open('GET','http://sriabi95.imad.hasura-app.io/counter',true);
     request.send(null);
-    
+};
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementbyId('submit_button');
+submit.onclick = function(){
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list <= '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementbyId('namelist');
+    ul.innerHTML = list;
 };
 
