766079992f925b9fd67dc1bef5b34cf37222d2af
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-27 20:54:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser'); // for obtaining body of post method
+var session = require('express-session');
 
 var config = {
    user : 'sriabi95',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: {maxage : 1000 * 60 * 60 * 24 * 30}
+}));
 
 /*var articles = {
     'article-one' : {
