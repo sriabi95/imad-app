e3fcb1ebb0c738b82176b4122481640ec6a57fe0
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-27 12:01:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,11 +140,24 @@ app.get('/submit-name',function (req, res){  //url : /submit-name?name=xxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == content object for article-one
- var articleName = req.params.articleName;
- res.send(createTemplate(articles[articleName]));
+ pool.query("SELECT * FROM articles WHERE title = "+ req.params.articleName, fuction(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     }
+     else{
+         if(result.rows.length === 0){
+             res.status(404).send("Article not found");
+         }
+         else{
+             var articleData = result.rows[0];
+             res.send(createTemplate(articleData));
+         }
+     }
+ });
+ 
 });
 
 
