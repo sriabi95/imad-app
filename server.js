4cc270931897cae1948645a6e043c7b316137ae4
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-08-27 20:57:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,6 +158,10 @@ app.post('/login',function(req,res){
             var hashedPassword = hash(password, salt);
             if(hashedPassword === dbString){
                 res.send('credentials are correct..');
+            
+                //set the session
+                
+                
             }
             else{
                 res.status(403).send('username/password is invalid');
