1c63f54343d86468ab7eb868693cfdd01c35c8f9
sriabi95 <31179432+sriabi95@users.noreply.github.com>
2017-09-16 15:09:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,48 +1,16 @@
 
-//counter increment reflected in server also
 
-var button = document.getElementById('counter');
 
-button.onclick = function(){
-//create a request object
-     var request = new XMLHttpRequest();
-     request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status == 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        } 
-     };
-    //make a request
-    request.open('GET','http://sriabi95.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-//submit name
+//submit username and password
 
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
     var request = new XMLHttpRequest();
      request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
-//            if(request.status === 200){      //200 is the successful response
-                /*var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();*/
-                
-                /*var names = request.responseText;
-                names = JSON.parse(names);
-                
-                var list = '';
-                for(var i=0; i<names.length; i++){
-                    list = list + '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;*/
-                
+
                 if(request.status === 200){
+                    console.log("user logged in");
                    alert('logged in successfully'); 
                 }
                 else if(request.status === 403){
@@ -51,7 +19,7 @@ submit.onclick = function(){
                 else if(request.status === 500){
                    alert('something went wrong on the server');
                 }
-//            }
+
         } 
      };
     //make a request
